38a9b43cc0373f19fa4f3220796dc8b759152b39
Itay Karo <karoitay@gmail.com>
2017-05-15 09:44:06 +0300

    remove mistakenly added /**/ from CalibratedSensor.h

--- CalibratedSensor.h
+++ CalibratedSensor.h
@@ -11,7 +11,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// CalibratedSensor.h/**/
+// CalibratedSensor.h
 
 #ifndef _CALIBRATEDSENSOR_h
 #define _CALIBRATEDSENSOR_h
