d22ebffb0f0fffa0e7b801d72043c17c65ed2968
Arik Yavilevich <arik@yavilevich.com>
2017-12-05 17:19:27 +0200

    fix insufficient discharge for ESP32 cap pressure sensor

--- SensorCapacitivePressure1PinESP32.cpp
+++ SensorCapacitivePressure1PinESP32.cpp
@@ -32,16 +32,35 @@ sensorVal_t SensorCapacitivePressure1PinESP32::getValueInt()
 	//noInterrupts(); // should we disble interrupts on the ESP32 during this measurement?
 
 	pinMode(mPin, INPUT_PULLUP);
-	uint32_t startCycle = xthal_get_ccount(); // we are measuring cycles here
+	//uint32_t startCycle = xthal_get_ccount(); // we are measuring cycles here
 	// Charge to a HIGH level, somewhere between 1.2V and 1.5V (empirical)
 	while (digitalRead(mPin) == LOW && --left>0); // we are counting number of loop iterations here (about 42 cycles per loop)
-	uint32_t endCycle = xthal_get_ccount();
-
-	// discharge
+	//uint32_t endCycle = xthal_get_ccount();
+	
+	// stop charge, start discharge
 	pinMode(mPin, INPUT_PULLDOWN);
-	delayMicroseconds(clockCyclesToMicroseconds(endCycle - startCycle)); // discharge for same time that was charged, but also leave as pull-down
 
+	// calculate loops
 	tests = TIMEOUT_ATTEMPTS - left; // how many loop iterations
+
+	// discharge duration
+	// we charge to about half Vsource, so t(charge)=ln(2)*RC => RC=t(charge)/ln(2)
+	// to discharge properly we should discharge for t(dis)=5RC=5*t(charge)/ln(2)=5*loops2us*42/1.44=5*60*loops2us
+	delayMicroseconds(clockCyclesToMicroseconds(5*60*tests)); // discharge for same time that was charged, but also leave as pull-down
+
+	// debug
+	// note: if this code runs, it will provide additional time for the cap to discharge. something that otherwise does not happen in a real scenario.
+	/*
+	if (DebugStream) {
+		DebugStream->print("SensorCapacitivePressure1PinESP32: ");
+		DebugStream->print(endCycle - startCycle);
+		DebugStream->print(", ");
+		DebugStream->print(tests);
+		DebugStream->print(", ");
+		DebugStream->println(clockCyclesToMicroseconds(endCycle - startCycle));
+	}
+	*/
+
 	return tests*GAIN;
 }
 
